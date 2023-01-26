b2ff8b3f47fa31efa1759bd08c18bf536ea8eeaa
cclutta <52520502+cclutta@users.noreply.github.com>
2023-01-26 23:15:45 +0300

    Create 0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""UTF8 Validator"""
+
+
+def validUTF8(data):
+    """Checks if data is valid UTF8
+    """
+
+    nbBytes = 0
+
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for byte in data:
+        mask = 1 << 7
+
+        if nbBytes == 0:
+            while byte & mask:
+                nbBytes += 1
+                mask = mask >> 1
+
+            if nbBytes == 0:
+                continue
+
+            if nbBytes == 1 or nbBytes > 4:
+                return False
+
+        else:
+            if not byte & mask1 and byte & mask2:
+                return False
+        nbBytes -= 1
+
+    return nbBytes == 0
