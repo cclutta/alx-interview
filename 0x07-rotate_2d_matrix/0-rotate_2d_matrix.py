3be7c7c6468cb5449da8f6bc1195f93d3b3ad998
cclutta <chelsea.lutta@gmail.com>
2023-02-16 09:50:15 +0300

    Update

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Rotate 2D Matrix module. """
+
+def rotate_2d_matrix(matrix):
+    """ Rotates 2D matrix in place. """
+    N = len(matrix[0])
+    for i in range(N // 2):
+        for j in range(i, N - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[N - 1 - j][i]
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
+            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
+            matrix[j][N - 1 - i] = temp
